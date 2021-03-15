57c31f979ec93d6ec5ad31086623cc7fcb5c5bfc
Karoline Lima <karol.lima012@gmail.com>
2021-03-14 23:03:04 -0300

    Prática - Trabalhando com objetos distribuídos e servidor com EJB

---
+++ src/main/java/com/gugawag/pdist/ejbs/MensagemDAO.java
@@ -0,0 +1,31 @@
+package com.gugawag.pdist.ejbs;
+
+import com.gugawag.pdist.model.Mensagem;
+import com.gugawag.pdist.model.Usuario;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class MensagemDAO {
+
+    @PersistenceContext(unitName="default")
+    private EntityManager em;
+
+    public void inserir(Mensagem novaMensagem){
+
+        em.persist(novaMensagem);
+    }
+
+    public List<Mensagem> listar() {
+
+        return em.createQuery("FROM Mensagem").getResultList();
+    }
+
+    public Mensagem pesquisarPorId(long id){
+        return (Mensagem) em.createQuery("FROM Mensagem msg WHERE m.id = :msgId")
+                .setParameter("msgId", id).getSingleResult();
+    }
+}
